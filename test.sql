9f0c7e3157920c5a319ee943be4b64aa00f4ba91
Andrei <andreii.lapschow1999@mail.ru>
2020-11-07 21:45:49 +0400

    1.3 insert education discipline

--- test.sql
+++ test.sql
@@ -2,12 +2,8 @@
  show tables;
  SET SQL_SAFE_UPDATES = 0;
  
- 
- update first_year
- set title='MatStat'
- where id_education_discipline=7;
- 
 
-
+INSERT INTO first_year(id_education_discipline,title,number_semestr,id_professor)
+VALUES (10,'C++','3','2');
  
 
