f9e66245f652196cf0498666f2310ddd35b83e46
Andrei <andreii.lapschow1999@mail.ru>
2020-11-07 22:31:51 +0400

    1.4

--- test.sql
+++ test.sql
@@ -3,7 +3,11 @@
  SET SQL_SAFE_UPDATES = 0;
  
 
-INSERT INTO first_year(id_education_discipline,title,number_semestr,id_professor)
-VALUES (10,'C++','3','2');
+create view professor_on_first_year(title, professor_name,professor_lname)
+as
+select fy.title , p.fname, p.lname 
+from first_year fy
+inner join professor p on p.id_professor=fy.id_professor
+
  
 
