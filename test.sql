25dc68ce32824f4ef3361c03f7755e8b93610370
Andrei <andreii.lapschow1999@mail.ru>
2020-11-07 21:36:49 +0400

    update first year

--- test.sql
+++ test.sql
@@ -2,14 +2,12 @@
  show tables;
  SET SQL_SAFE_UPDATES = 0;
  
-create algorithm=temptable
-view after_1999_studet
-as 
-select s.fname,s.lname,ed.title ,m.value_mark from mark m 
-inner join student s on s.st_book_id=m.st_book_id
-inner join education_discipline ed on ed.id_education_discipline=m.id_education_discipline
-where m.st_book_id in  
-(select s.st_book_id  from student s
-where year(s.birth_date)>1999);
+ 
+ update first_year
+ set title='MatStat'
+ where id_education_discipline=7;
+ 
+
+
  
 
