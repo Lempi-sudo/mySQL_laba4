df7146acd43bcbacaa53a3563652f0adb0ef4b9d
Andrei <andreii.lapschow1999@mail.ru>
2020-11-08 15:48:50 +0400

    clear test

--- test.sql
+++ test.sql
@@ -1,14 +1,3 @@
-use bank;
-SET SQL_SAFE_UPDATES = 0;
- 
-create view	only_man_person
-as
-select  p.fname, p.lname from person p 
-where p.gender='M';
 
-UPDATE only_man_person SET fname='Andrei'
-where fname='Andrey';
-
-INSERT INTO  only_man_person(fname,lname) VALUES ('petr','gaf');
 
 
