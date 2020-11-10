db909b51d4ab22b8a7c1364de40d68cb6ea06317
Andrei <andreii.lapschow1999@mail.ru>
2020-11-10 18:58:00 +0400

    trigger 1.0

---
+++ trigger.sql
@@ -0,0 +1,133 @@
+use football_league;
+
+#(2.1)  Создать триггер, который при удалении записи из одной 
+#таблицы удаляет все связанные с ней данные из других таблиц. (AFTER DELETE)
+
+delimiter //
+create trigger delete_player after delete on team
+for each row
+begin
+	delete from  player p where p.id_team=OLD.id_team;
+end;//
+delimiter ;
+
+delete from team 
+where id_team=3;
+
+
+#2.2 - Создать триггер, выполняющий проверку изменяемых данных (BEFORE UPDATE)
+delimiter //
+create trigger upd_check_goals_and_assist BEFORE UPDATE ON player
+for each row
+begin
+	if new.goals<0 then
+		set new.goals=0;
+	end if;
+	if new.assist<0 then
+		set new.assist=0;
+	end if;
+end ;//
+delimiter ;
+
+
+delimiter //
+create trigger upd_check_place BEFORE UPDATE ON team
+for each row
+begin
+	if new.place<=0 then
+		set new.place=1;
+	elseif new.place>20 then
+		set new.place=20;
+	end if;
+end ;//
+delimiter ;
+
+
+#2.3 Создать триггер, заполняющий одно из полей таблицы на основе вводимых данных 
+#(например, поле «Возраст» по вводимым данным поля «Дата рождения»). (BEFORE INSERT)
+
+delimiter //
+create trigger agе before insert on player
+for each row
+begin
+	if NEW.birth_data is not null then
+		insert into information_about_player(Age,idPlayer) value((YEAR(CURRENT_DATE) - YEAR(NEW.birth_data))
+        -(DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(NEW.birth_data, '%m%d')),new.idPlayer);
+	end if;
+	end;//
+delimiter ;
+
+
+
+#2.4 Создать два триггера: первый осуществляет проверку целостности добавляемых данных в таблицу, 
+#второй запрещает добавление данных при заданных условиях. (BEFORE INSERT, BEFORE INSERT).
+
+delimiter //
+create trigger check_is_not_null BEFORE INSERT ON player
+for each row 
+begin
+	if new.fname is null or new.lname is null or new.id_team is null
+	or new.birth_data is null or new.goals is null or new.assist is null then 
+		signal sqlstate '45000' set message_text = 'data is null';
+	end if ;
+	if new.idPlayer<0 then 
+		signal sqlstate '45000' set message_text = 'fatal: id_Player<0';
+	end if;
+end;//
+delimiter ;
+
+delimiter //
+create trigger check_is_data_validation BEFORE INSERT ON player
+for each row follows check_is_not_null
+begin
+	if LENGTH(new.fname)=0 OR LENGTH(new.fname)>45 OR LENGTH(new.lname)=0 OR LENGTH(new.lname)>45 then
+		signal sqlstate '45000' set message_text = 'invalid name1';
+	end if ;
+	if new.id_team>20 or  new.id_team<=0 then 
+		signal sqlstate '45000' set message_text = 'invalid id_team';
+	end if ;
+    if  new.birth_data>CURRENT_DATE or new.birth_data<'1960-01-01' then
+		signal sqlstate '45000' set message_text = 'data out of range';	
+	end if;
+    if new.goals<=0 or new.assist<=0 then
+		signal sqlstate '45000' set message_text = 'negativ number';	
+	end if;
+end;//
+delimiter ;
+
+
+
+
+#2.5 Создать триггеры, осуществляющие протоколирование операций INSERT, UPDATE, DELETE для заданной таблицы.
+# Для этого создать новую таблицу, содержащую следующие поля: 
+#пользователь, операция, время, данные до выполнения операции, новые данные.
+
+delimiter //
+create trigger delete_protocol after delete ON team
+for each row 
+begin
+	insert into protocol(id_user,action,time,old_id_team,new_id_team,old_title,new_title,old_place,new_place,old_city,new_city) 
+    value( USER() , 'DELETE' , CURRENT_TIMESTAMP() , old.id_team , null , old.tittle , null , old.place , null , old.city , null);
+end;//
+delimiter ;
+
+
+delimiter //
+create trigger update_protocol after update ON team
+for each row 
+begin
+	insert into protocol(id_user,action,time,old_id_team,new_id_team,old_title,new_title,old_place,new_place,old_city,new_city) 
+    value( USER() , 'UPDATE' , CURRENT_TIMESTAMP() , old.id_team , new.id_team , old.tittle , new.tittle , old.place , new.place , old.city , new.city);
+end;//
+delimiter ;
+
+
+delimiter //
+create trigger insert_protocol after insert ON team
+for each row 
+begin
+	insert into protocol(id_user,action,time,old_id_team,new_id_team,old_title,new_title,old_place,new_place,old_city,new_city) 
+    value( USER() , 'INSERT' , CURRENT_TIMESTAMP() , null , new.id_team , null , new.tittle , null , new.place , null , new.city);
+end;//
+delimiter ;
+
