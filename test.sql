63e17a96bc9e0b6859cf669c54cafcdf513c5f43
Andrei <andreii.lapschow1999@mail.ru>
2020-11-10 11:00:08 +0400

    2.4

--- test.sql
+++ test.sql
@@ -57,3 +57,44 @@ begin
 	end;//
 delimiter ;
 
+
+
+#2.4 Создать два триггера: первый осуществляет проверку целостности добавляемых данных в таблицу, 
+#второй запрещает добавление данных при заданных условиях. (BEFORE INSERT, BEFORE INSERT).
+
+delimiter //
+create trigger check_is_not_null BEFORE INSERT ON player
+for each row 
+begin
+	if new.fname is null or new.lname is null or new.id_team is null
+	or new.birth_data is null or new.goals is null or new.assist is null then 
+		signal sqlstate '45000' set message_text = 'data is null';
+	end if ;
+	if new.idPlayer<0 then 
+		signal sqlstate '45000' set message_text = 'fatal: id_Player<0';
+	end if;
+end;//
+delimiter ;
+
+delimiter //
+create trigger check_is_data_validation BEFORE INSERT ON player
+for each row follows check_is_not_null
+begin
+	if LENGTH(new.fname)=0 OR LENGTH(new.fname)>45 OR LENGTH(new.lname)=0 OR LENGTH(new.lname)>45 then
+		signal sqlstate '45000' set message_text = 'invalid name1';
+	end if ;
+	if new.id_team>20 or  new.id_team<=0 then 
+		signal sqlstate '45000' set message_text = 'invalid id_team';
+	end if ;
+    if  new.birth_data>CURRENT_DATE or new.birth_data<'1960-01-01' then
+		signal sqlstate '45000' set message_text = 'data out of range';	
+	end if;
+    if new.goals<=0 or new.assist<=0 then
+		signal sqlstate '45000' set message_text = 'negativ number';	
+	end if;
+end;//
+delimiter ;
+
+
+
+
